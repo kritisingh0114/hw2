33fa8a6019e709264a58f047d53891744143e9e1
Kriti Singh <90220396+kritisingh0114@users.noreply.github.com>
2023-10-18 20:04:57 -0400

    Edited Transaction.java to establish data encapsulation and immutability

--- expense_tracker/src/model/Transaction.java
+++ expense_tracker/src/model/Transaction.java
@@ -4,11 +4,11 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-public class Transaction {
+public final class Transaction {
 
-  public double amount;
-  public String category;
-  public String timestamp;
+  private double amount;
+  private String category;
+  private String timestamp;
 
   public Transaction(double amount, String category) {
     this.amount = amount;
@@ -20,24 +20,24 @@ public class Transaction {
     return amount;
   }
 
-  public void setAmount(double amount) {
-    this.amount = amount;
-  }
+  // public void setAmount(double amount) {
+  //   this.amount = amount;
+  // }
 
   public String getCategory() {
     return category;
   }
 
-  public void setCategory(String category) {
-    this.category = category; 
-  }
-  
+  // public void setCategory(String category) {
+  //   this.category = category; 
+  // }
+
   public String getTimestamp() {
     return timestamp;
   }
 
   private String generateTimestamp() {
-    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm");  
+    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm");
     return sdf.format(new Date());
   }
 
